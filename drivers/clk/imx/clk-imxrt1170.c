14d4197312bf6dfe7d88e1ae36da04b333ab8fa3
Alexander Sanochkin <sasha@emcraft.com>
2023-11-10 06:04:34 +0000

    RM-6903: clk: imart1170: Support IMXRT1170 ADC clocks.

--- drivers/clk/imx/clk-imxrt1170.c
+++ drivers/clk/imx/clk-imxrt1170.c
@@ -35,6 +35,10 @@ static const char * const lpi2c1_4_sels[] = {IMXRT1170_CLK_SRC_COMMON,
 "pll3_div2", "pll1_div5", "pll2_sys", "pll2_pfd3"};
 static const char * const lpi2c5_6_sels[] = {IMXRT1170_CLK_SRC_COMMON,
 "pll3_pfd3", "pll3_sys", "pll2_pfd3", "pll1_div5"};
+static const char * const adc1_sels[] = {IMXRT1170_CLK_SRC_COMMON,
+"pll3_div2", "pll1_div5", "pll3_pfd2", "pll3_pfd3"};
+static const char * const adc2_sels[] = {IMXRT1170_CLK_SRC_COMMON,
+"pll3_div2", "pll1_div5", "pll3_pfd2", "pll3_pfd3"};
 
 struct clk_hw *imxrt1170_clk_pll_div_out_composite(const char *name, const char *parent_name,
 						void __iomem *reg, int div_factor, int gate_bit, unsigned long flags)
@@ -87,6 +91,8 @@ static struct imxrt1170_clk_root clk_roots[] = {
 	{ IMXRT1170_CLK_ROOT_BUS, "bus_root", bus_sels, (2 * 0x80), CLK_IS_CRITICAL },
 	{ IMXRT1170_CLK_ROOT_BUS_LPSR, "bus_lpsr_root", bus_lpsr_sels, (3 * 0x80), CLK_IS_CRITICAL },
 	{ IMXRT1170_CLK_ROOT_SEMC, "semc_root", semc_sels, (4 * 0x80), CLK_IS_CRITICAL },
+	{ IMXRT1170_CLK_ROOT_ADC1, "adc1_root", adc1_sels, (9 * 0x80),  },
+	{ IMXRT1170_CLK_ROOT_ADC2, "adc2_root", adc2_sels, (10 * 0x80),  },
 	{ IMXRT1170_CLK_ROOT_GPT1, "gpt1_root", gpt1_sels, (14 * 0x80), },
 	{ IMXRT1170_CLK_ROOT_LPUART1, "lpuart1_root", lpuart1_sels, (25 * 0x80), },
 	{ IMXRT1170_CLK_ROOT_LPI2C1, "lpi2c1_root", lpi2c1_4_sels, (37 * 0x80), },
@@ -111,6 +117,8 @@ static struct imxrt1170_clk_ccgr clk_ccgrs[] = {
 	{ IMXRT1170_CLK_M7, "m7", "m7_root", 0x6000, CLK_IS_CRITICAL },
 	{ IMXRT1170_CLK_EDMA, "edma", "bus_root", (0x6000 + (20 * 0x20)) },
 	{ IMXRT1170_CLK_SEMC, "semc", "semc_root", (0x6000 + (33 * 0x20)), CLK_IS_CRITICAL },
+	{ IMXRT1170_CLK_ADC1, "adc1", "adc1_root", (0x6000 + (55 * 0x20)), },
+	{ IMXRT1170_CLK_ADC2, "adc2", "adc2_root", (0x6000 + (56 * 0x20)), },
 	{ IMXRT1170_CLK_GPT1, "gpt1", "gpt1_root", (0x6000 + (64 * 0x20)), },
 	{ IMXRT1170_CLK_LPUART1, "lpuart1", "lpuart1_root", (0x6000 + (86 * 0x20)), },
 	{ IMXRT1170_CLK_LPI2C1, "lpi2c1", "lpi2c1_root", (0x6000 + (98 * 0x20)), },
